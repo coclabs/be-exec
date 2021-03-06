c8d875b330956f8c256b3370adad2c40b1895660
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2021-03-06 08:16:22 +0700

    maint(project): Rename tests/target to tests/module

--- coc_python/__main__.py
+++ coc_python/__main__.py
@@ -4,7 +4,7 @@ import testscore
 
 if __name__ == '__main__':
 
-    with open('tests/target/my_module.py', 'rb') as code_module, open('tests/target/test_case.py', 'rb') as test_module:
+    with open('tests/module/my_module.py', 'rb') as code_module, open('tests/module/test_case.py', 'rb') as test_module:
         _code_object: Final = compile(code_module.read(), f'<main>', 'exec', optimize=2)
         _test_code_object: Final = compile(test_module.read(), f'<test>', 'exec', optimize=2)
         exec(_code_object)
