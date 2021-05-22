b219d673c142c4477f3a4fe10c6bcc0ecab05594
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2021-05-23 01:43:34 +0700

    ADD sentry integration

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import api
+import sentry_sdk
 
 from fastapi import FastAPI
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+
+sentry_sdk.init(
+    dsn="https://957e126643af407c94a99b32c263025d@o708708.ingest.sentry.io/5778759",
+    traces_sample_rate=1.0
+)
 
 
 @api.init_routes
@@ -8,6 +15,7 @@ class App:
     """An application class for simple FastAPI application"""
 
     service = FastAPI()
+    service.add_middleware(SentryAsgiMiddleware)
 
     def __new__(cls): pass
 
