62ee0babdfc327ec7ec4fdf3a4021613005bd975
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2021-05-23 04:09:54 +0700

    Revert "FIX unawaited service called"
    This reverts commit b8ce1390081f88cd512bccffe2a8a425d6f4075d.

--- api/v1/controllers.py
+++ api/v1/controllers.py
@@ -3,7 +3,7 @@ from services.code_execute_service import ExecuteService
 
 
 async def execute_code(item: Code):
-    result = await ExecuteService(item.code, item.context).service()
+    result = ExecuteService(item.code, item.context).service()
     response_data = {
             'errors': result.errors,
             'failures': result.failures,
