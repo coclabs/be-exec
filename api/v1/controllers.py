b8ce1390081f88cd512bccffe2a8a425d6f4075d
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2021-05-23 03:57:35 +0700

    FIX unawaited service called

--- api/v1/controllers.py
+++ api/v1/controllers.py
@@ -3,7 +3,7 @@ from services.code_execute_service import ExecuteService
 
 
 async def execute_code(item: Code):
-    result = ExecuteService(item.code, item.context).service()
+    result = await ExecuteService(item.code, item.context).service()
     response_data = {
             'errors': result.errors,
             'failures': result.failures,
