32cce7974d123a4ceb4ec6d760187907366f5d39
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2021-05-29 14:38:19 +0700

    FIX commented sentry_sdk

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import sentry_sdk
 from fastapi import FastAPI
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
-# sentry_sdk.init(
-#     dsn='https://12369f77d5474acdb6908dbeb8daddaf@o525207.ingest.sentry.io/5778800',
-#     traces_sample_rate=1.0,
-#     environment='develop'
-# )
+sentry_sdk.init(
+    dsn='https://12369f77d5474acdb6908dbeb8daddaf@o525207.ingest.sentry.io/5778800',
+    traces_sample_rate=1.0,
+    environment='develop'
+)
 
 
 @api.init_routes
@@ -16,7 +16,7 @@ class App:
     """An application class for simple FastAPI application"""
 
     service = FastAPI()
-    # service.add_middleware(SentryAsgiMiddleware)
+    service.add_middleware(SentryAsgiMiddleware)
 
     def __new__(cls): pass
 
