4cd5ff004e9f243340d705043e6f9151e7e98f65
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2021-05-29 13:47:49 +0700

    FIX cannot unpack non-iterable SupportedHTTPMethod object

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -6,7 +6,8 @@ def init_routes(app: Typing.App, /):
 
     router = routes.Router
     for route, route_attr in router.routes.items():
-        for method, method_attr in route_attr.get('methods'):
-            router.register_route(app.service, route, method=method, **method_attr)
+        for method, method_attr in route_attr.get('methods').items():
+            callback, args = method_attr.get('callback'), method_attr.get('args', {})
+            router.register_route(app.service, route, method=method, callback=callback, **args)
 
     return app
